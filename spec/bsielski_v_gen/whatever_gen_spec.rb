357a7f96cf33dd2acc9de56fdc4df7304a2f74b6
Bartłomiej Sielski <b.sielski@yandex.com>
2018-09-04 19:54:45 +0200

    Fix double tests

--- spec/bsielski_v_gen/whatever_gen_spec.rb
+++ spec/bsielski_v_gen/whatever_gen_spec.rb
@@ -28,29 +28,6 @@ RSpec.describe VGen::WhateverGen do
       end
     end
 
-    context "called without gens" do
-      subject {
-        Array.new(300) {
-          described_class.new.()
-        }
-      }
-
-      it "contains only floats" do
-        only_floats = subject.all? {|f| f.is_a?(Float)}
-        expect(only_floats).to be true        
-      end
-
-      it "only smaller or equal to than 1" do
-        only_less_than_1 = subject.all? { |f| f <= 1 }
-        expect(only_less_than_1).to be true        
-      end
-
-      it "only bigger or equal to than 0" do
-        only_greater_than_0 = subject.all? { |f| f >= 0 }
-        expect(only_greater_than_0).to be true        
-      end
-    end
-
     context "called with three custom gens" do
       subject {
         Array.new(300) {
